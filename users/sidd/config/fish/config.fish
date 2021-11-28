9f0eee7e391a9c528eb7e0cfd7dfc82b8df14d6a
siddhantk232 <siddhantk232@gmail.com>
2021-11-28 12:48:53 +0530

    user scripts

--- users/sidd/config/fish/config.fish
+++ users/sidd/config/fish/config.fish
@@ -7,25 +7,21 @@ set -x BROWSER brave
 set -x TERM st
 set -x LC_ALL en_US.UTF-8
 set -x BAT_THEME gruvbox-dark
+set -x PATH $PATH ~/.dotfiles/users/sidd/config/scripts
 
 # config aliases (all lowercase and starts with c)
-alias cfish        "nvim ~/.config/fish/config.fish"
-alias ci3          "nvim ~/.config/i3/config"
-alias ci3status    "nvim ~/.config/i3status-rust/config.toml"
-alias ctmux        "nvim ~/.tmux.conf"
-alias csessions    "nvim ~/.config/scripts/sessions"
-alias cth          "wal -i ~/Pictures/Wallpapers"
+alias config "nvim ~/.dotfiles"
 
 # aliases for commands
 alias r            "./bin/rails"
 alias e            "nvim"
 alias ls           "lsd"
-alias cwd          "cd ~/projects/work"
 alias tmux         "tmux -2" # force 256 color
 alias ph           "kdeconnect-cli -d f9118946111fd280"
 
 # keybinds
-# bind -M insert \cr 'sessions'
+bind -M insert \cp 'sessions'
+bind -M insert \ce 'pdfs'
 
 # enable vi
 fish_vi_key_bindings
