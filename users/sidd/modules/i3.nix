d0db9a562d1fd7444dbed88d05ef470b5d81485e
siddhantk232 <siddhantk232@gmail.com>
2021-12-08 20:00:53 +0530

    custom st setup
    uses https://github.com/siddhantk232/st

--- users/sidd/modules/i3.nix
+++ users/sidd/modules/i3.nix
@@ -2,5 +2,7 @@
 {
   xdg.configFile."i3/config".text = builtins.readFile ../config/i3/config;
   xdg.configFile."i3status-rust/config.toml".text = builtins.readFile ../config/i3/i3status-rs.toml;
-  home.packages = [ pkgs.lukesmithxyz-st ];
+  home.packages = [ 
+    (pkgs.callPackage ../packages/st.nix { })
+  ];
 }
---
+++ users/sidd/packages/st.nix
@@ -0,0 +1,33 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation rec {
+  pname   = "siddhantk232-st";
+  version = "1.0.0";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "siddhantk232";
+    repo = "st";
+    rev = "76b6e1a6501af0cfd89d9652949e93122accab2b";
+    hash = "sha256-xiyJMLfmPDctW+pa4LRnddoZW71cGoBGrDorgTJB2NE=";
+  };
+
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+  ];
+
+  buildInputs = with pkgs; [
+    fontconfig
+    harfbuzz
+    xorg.libX11
+    xorg.libXext
+    xorg.libXft
+    ncurses
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    TERMINFO=$out/share/terminfo make install PREFIX=$out
+
+    runHook postInstall
+  '';
+}
