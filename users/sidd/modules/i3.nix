c134384fb05c99549361b58875894df2e0d9dace
siddhantk232 <siddhantk232@gmail.com>
2021-12-18 19:28:51 +0530

    rofi and go 17

--- users/sidd/modules/i3.nix
+++ users/sidd/modules/i3.nix
@@ -4,5 +4,7 @@
   xdg.configFile."i3status-rust/config.toml".text = builtins.readFile ../config/i3/i3status-rs.toml;
   home.packages = [ 
     (pkgs.callPackage ../packages/st.nix { })
+    pkgs.rofi
+    pkgs.rofi-emoji
   ];
 }
--- users/sidd/modules/languages.nix
+++ users/sidd/modules/languages.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    go
+    go_1_17
     nodejs-16_x
     haskellPackages.ghc
     gcc
