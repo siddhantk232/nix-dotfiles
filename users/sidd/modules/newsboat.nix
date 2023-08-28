a77fc82b5886feec82e50cdd113b70f263d1695a
siddhantCodes <siddhantk232@gmail.com>
2023-08-28 22:45:40 +0530

    new blogs

--- users/sidd/modules/newsboat.nix
+++ users/sidd/modules/newsboat.nix
@@ -5,16 +5,11 @@
   ];
 
   home.file.".newsboat/urls".text = ''
-    https://siddhantcodes.netlify.app/feed.xml
     https://blogs.nasa.gov/webb/feed/
-    https://monicalent.com/blog/index.xml
-    https://hnrss.org/jobs
-    https://hnrss.org/bestcomments
-    https://hnrss.org/frontpage
     https://fasterthanli.me/index.xml
-    https://xn--i2r.xn--rhqv96g/rss.xml
-    https://world.hey.com/dhh/feed.atom
     http://jackkelly.name/blog/rss.xml
     https://lexi-lambda.github.io/feeds/all.rss.xml
+    https://blog.ncase.me/rss/
+    https://www.scattered-thoughts.net/atom.xml
   '';
 }
